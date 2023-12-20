b1b85b01f0f262059c3fc40cac608c284335be62
nikolay <kolyan083@yandex.ru>
2023-12-20 15:24:07 +0300

    Initial commit1

--- App.py
+++ App.py
@@ -1,5 +1,5 @@
 from select import select
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login.utils import _secret_key
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import False_
@@ -15,24 +15,40 @@ db = SQLAlchemy(app)
 app.app_context().push()
 
 
+# @app.route('/create', methods=['GET', 'POST'])
+# def create():
+#     if request.method == 'GET':
+#         return render_template('create.html')
+#
+#
+# app.run(host='localhost', port=5000)
+
 class SVT(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     typesvt = db.Column(db.String(100))
     otd = db.Column(db.String(100))
     mol = db.Column(db.String(100))
     inv = db.Column(db.String(100))
+    serial = db.Column(db.String(100))
+    namedomain = db.Column(db.String(100))
+    nameuser = db.Column(db.String(100))
+    typeos = db.Column(db.String(100))
 
-
-    def __init__(self, typesvt, otd, mol, inv):
+    def __init__(self, typesvt, otd, mol, inv, serial, namedomain, nameuser, typeos):
         self.typesvt = typesvt
         self.otd = otd
         self.mol = mol
         self.inv = inv
+        self.serial = serial
+        self.namedomain = namedomain
+        self.nameuser = nameuser
+        self.typeos = typeos
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    all_svt = SVT.query.all()
+    return render_template("index.html", employees=all_svt)
 
 
 @app.route('/insert', methods=['POST'])
@@ -42,10 +58,34 @@ def insert():
         otd = request.form['otd']
         mol = request.form['mol']
         inv = request.form['inv']
+        serial = request.form['serial']
+        namedomain = request.form['namedomain']
+        nameuser = request.form['nameuser']
+        typeos = request.form['typeos']
 
-        my_svt = SVT (typesvt, otd, mol, inv)
+        my_svt = SVT(typesvt, otd, mol, inv, serial, namedomain, nameuser, typeos)
         db.session.add(my_svt)
         db.session.commit()
+        return redirect(url_for('index'))
+
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    if request.method == 'POST':
+        my_svt = SVT.query.get(request.form.get('id'))
+
+        my_svt.typesvt = request.form['typesvt']
+        my_svt.otd = request.form['otd']
+        my_svt.mol = request.form['mol']
+        my_svt.inv = request.form['inv']
+        my_svt.serial = request.form['serial']
+        my_svt.namedomain = request.form['namedomain']
+        my_svt.nameuser = request.form['nameuser']
+        my_svt.typeos = request.form['typeos']
+
+        db.session.commit()
+        flash("Запись успешно обновлена")
+
         return redirect(url_for('index'))
 
 
